43bcfab747240769b6abe60b9592e040ce046fc5
thndlovu <tndlovu@student.ubc.ca>
2025-03-02 22:00:10 -0800

    Initialized Plane, Player object and platforms as well as basic movement functionality.
    Created the scene of the game with the platforms and the player. Added basic character movement to the player object as well as FreeLook camera.

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -2,10 +2,27 @@ using UnityEngine;
 using UnityEngine.Events;
 
 public class InputManager : MonoBehaviour {
-    public UnityEvent<Vector2> OnMove = new UnityEvent<Vector2>();
+    public UnityEvent<Vector3> OnMove = new UnityEvent<Vector3>();
     public UnityEvent OnSpacePressed = new UnityEvent();
     // Update is called once per frame
     void Update(){
-        if(Input.GetKeyDpown)
+        if(Input.GetKeyDown(KeyCode.Space)){
+            OnSpacePressed?.Invoke();
+        }
+
+        Vector3 input = Vector3.zero;
+        if(Input.GetKey(KeyCode.A)){
+            input += Vector3.left;
+        }
+        if(Input.GetKey(KeyCode.D)){
+            input += Vector3.right;
+        }
+        if(Input.GetKey(KeyCode.S)){
+            input += Vector3.back;
+        }
+        if(Input.GetKey(KeyCode.W)){
+            input += Vector3.forward;
+        }
+        OnMove?.Invoke(input);
     }
 }
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour{
+    [SerializeField] private InputManager inputManager;
+    [SerializeField] private float speed;
+    [SerializeField] private float jumpForce;
+    private bool isGrounded;
+    private Rigidbody rb;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start(){
+        inputManager.OnMove.AddListener(MovePlayer);
+        inputManager.OnSpacePressed.AddListener(Jump);
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void MovePlayer(Vector3 direction){
+        Vector3 moveDirection = new(direction.x, 0f, direction.z);
+        rb.AddForce(speed * moveDirection);
+    }
+
+    private void Jump(){
+        if(isGrounded) {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isGrounded = false; 
+        }
+    }
+}
